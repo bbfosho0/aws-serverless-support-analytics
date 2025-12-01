e938e049f0367cd44edf1c16426dbca85a86a02b
Yoshi Rafael Gomez Gutierrez <32444740+bbfosho0@users.noreply.github.com>
2025-11-30 20:42:50 -0600

    Update ThemeProvider to set default and forced theme to dark

--- frontend/src/providers/theme-provider.tsx
+++ frontend/src/providers/theme-provider.tsx
@@ -5,7 +5,12 @@ import type { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      forcedTheme="dark"
+      {...props}
+    >
       {children}
     </NextThemesProvider>
   );
